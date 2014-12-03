3761a810901076ccaba22845aa9483f8c1b36db8
Seiji Nishiguchi <ninjhaaa@gmail.com>
2014-12-03 19:28:08 +0900

    for での配列処理をやめた

--- PIRSensor/server/cw.js
+++ PIRSensor/server/cw.js
@@ -1,20 +1,20 @@
 var request = require('request');
-var room_id = 'number of room id';
+var room_id = '{room id number}';
 var options = {
     headers: {
-        'X-ChatWorkToken': 'api token'
+        'X-ChatWorkToken': '{chatwork api token}'
     },
     from: [],
     json: true
 };
 var serial_server = {
-   url: 'url of serialport server',
+   url: 'http://localhost:{port-number}',
    from: [],
    json: true
 };
 var sensor_output;
 
-function getMessage(callback){
+function getMessages(callback){
     options.url = 'https://api.chatwork.com/v1/rooms/'+room_id+'/messages?force=0';
     request.get(options, function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -60,43 +60,47 @@ var bot_processing = function(){
         ask_pattern_2 = 'セコムええか？';
 
     //メッセージ取得
-    getMessage(function(data){
-        if(!Array.isArray(data))return;
-        console.log('check:',data);
-        console.log('sensor output is ' + sensor_output);
+    getMessages(function(messages){
+        if(!Array.isArray(messages))return;
+        //console.log('check:',messages);
+        //console.log('sensor output is ' + sensor_output);
 
         //未読メッセージ分繰り返し
-        for(var i = 0, length = data.length; i < length; i++) {
-            var tmp = data[i].body;
-            //発言者へのreply設定
-            var reply_message = '[To:' + data[i].account.account_id + '] ' + data[i].account.name + 'さん\n';
+        messages.map(function(message) {
+            var body = message.body;
+            console.log(body);
+            var result = '[To:' + message.account.account_id + '] ' + message.account.name + 'さん\n';
 
-            //ask_patternとマッチしたらセンサー入力に応じてreply_messageをセット
-            if(tmp.indexOf(ask_pattern_1) != -1) {
+            //ask_patternとマッチしたらセンサー入力に応じて返信メッセージ result をセット
+            if(body.indexOf(ask_pattern_1) != -1) {
                 if (sensor_output === 1) {
-                    reply_message += 'おるで';
+                    result += 'おるで';
                 } else if (sensor_output === 0) {
-                    reply_message += 'おらんで';
+                    result += 'おらんで';
                 } else {
-                    reply_message += '自分で確認してやー';
+                    result += '自分で確認してやー';
                 }
-            } else if (tmp.indexOf(ask_pattern_2) != -1){
+            } else if (body.indexOf(ask_pattern_2) != -1){
                 if (sensor_output === 1) {
-                    reply_message += 'セコムまだアカンで';
+                    result += 'セコムまだアカンで';
                 } else if (sensor_output === 0) {
-                    reply_message += 'セコムええで';
+                    result += 'セコムええで';
                 } else {
-                    reply_message += '自分で確認してやー';
+                    result += '自分で確認してやー';
                 }
             } else {
                 return;
             }
-
-            postMessage(reply_message, function(data){
+            return result;
+        }).filter(function(message) {
+            //mapでreturn;した要素を排除
+            return message;
+        }).forEach(function(message) {
+            postMessage(message, function(data){
                 if(!data)return;
                 console.log(data);
             });
-        }
+        });
     });
     setTimeout(bot_processing, 5000);
 };
