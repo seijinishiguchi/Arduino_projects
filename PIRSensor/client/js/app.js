a31f1489b5e65e4ed4a06453c3ba3a012d2b6c5c
Seiji Nishiguchi <ninjhaaa@gmail.com>
2014-12-03 19:30:19 +0900

    viewの変更を関数に切り出した

--- PIRSensor/client/js/app.js
+++ PIRSensor/client/js/app.js
@@ -1,25 +1,29 @@
 $(function(){
 
-    var serial_server = '*********';
+    var serial_server = 'http://localhost:{port-number}';
+
+    function viewChange(sensor_output) {
+        // センサー出力に応じてクラスを追加しテキストを差し替える
+        if (sensor_output === 1) {
+            $('.indicate').html('セコムまだアカンで');
+            $('.icon').attr('src', 'image/stop.svg');
+            $('body').addClass('ocupied');
+        } else if (sensor_output === 0) {
+            $('.indicate').html('セコムええで');
+            $('.icon').attr('src', 'image/check.svg');
+            $('body').removeClass('ocupied');
+        }
+    }
 
     // 1秒間隔でArduinoからセンサー出力を取得
     var serial_communication = function(){
-        $.getJSON(serial_server, {}, function(data){
-            //console.log(data);
-            var sensor_output = data[0].data;
-            console.log(sensor_output);
+        $.getJSON(serial_server)
+        .done(function (serial_data) {
+            //console.log('done', serial_data);
+            var sensor_output = serial_data[0].data;
+            viewChange(sensor_output);
 
-            // センサー出力に応じてクラスを追加しテキストを差し替える
-            if (sensor_output === 1) {
-                $('.indicate').html('セコムまだアカンで');
-                $('.icon').attr('src', 'image/stop.svg');
-                $('body').addClass('ocupied');
-            } else if (sensor_output === 0) {
-                $('.indicate').html('セコムええで');
-                $('.icon').attr('src', 'image/check.svg');
-                $('body').removeClass('ocupied');
-            }
-        });
+        }).fail(console.error.bind(console));
         setTimeout(serial_communication, 1000);
     };
     serial_communication();
